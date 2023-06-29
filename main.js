b5a2b87aef53d9190b39186071bd44ca4bde65c9
timothy <timothy.ndarau@student.moringaschool.com>
2023-06-29 14:39:24 +0300

    liker app done

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeglyph = document.querySelectorAll(".like-glyph");
 
+function likeCallback(e) {
+  const heart = e.target;
+  mimicServerCall("testUrl")
+    .then(() => {
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(error => {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
 
-
-
+// for (const glyph of likeglyph) {
+//   glyph.addEventListener("click", likeCallback);
+// }
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
